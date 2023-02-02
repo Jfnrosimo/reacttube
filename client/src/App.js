8f16af48b90a207e8ba4205c8521660103fc3920
Jfnrosimo <j.rosimo098.com>
2023-02-02 12:56:02 +0800

    Feat: added styled components for menu

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,42 @@ function App() {
       <Menu />
       <Main>
         <Navbar />
-        <Wrapper>video cards</Wrapper>
+        <Wrapper>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+          <h1>test</h1>
+        </Wrapper>
       </Main>
     </Container>
   );
