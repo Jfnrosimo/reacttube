460d08aa332db53126e212f83d607d1776c276db
Jfnrosimo <j.rosimo098.com>
2023-02-05 17:50:15 +0800

    Feat: added models for entities

--- api/index.js
+++ api/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
+const connect = () => {
+  mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => {
+      console.log("Connected to DB");
+    })
+    .catch((error) => console.log(error));
+};
+
 app.listen(8000, () => {
-  console.log("Connected!");
+  connect();
+  console.log("Connected! and Server");
 });
