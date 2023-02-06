1dd932eb5a3d0804b7e8991d06ed8eb9c98cf81e
Jfnrosimo <j.rosimo098.com>
2023-02-06 11:48:57 +0800

    Feat: added sigin controller functionality

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
-import User from "../models/User.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+import User from "../models/User.js";
+import { createError } from "../error.js";
 
 export const signup = async (req, res, next) => {
   try {
@@ -10,6 +13,20 @@ export const signup = async (req, res, next) => {
 
     await newUser.save();
     res.status(200).send("User has been created!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ name: req.body.name });
+    if (!user) return next(createError(404, "User not found!"));
+
+    const isCorrect = await bcrypt.compare(req.body.password, user.password);
+    if (!isCorrect) return next(createError(400, "Wrong credentials!"));
+
+    const token = jwt.sign({ id: user._id });
   } catch (error) {
     next(error);
   }
