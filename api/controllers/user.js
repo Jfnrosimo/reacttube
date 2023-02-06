5b9287e0d6f14c8f9d3fc313d6188d5eeb8edc91
Jfnrosimo <j.rosimo098.com>
2023-02-06 13:39:15 +0800

    Feat: added delete user controller

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,7 +1,39 @@
-export const updateUser = (req, res, next) => {};
-export const deleteUser = (req, res, next) => {};
-export const getUser = (req, res, next) => {};
-export const subscribe = (req, res, next) => {};
-export const unsubscribe = (req, res, next) => {};
-export const like = (req, res, next) => {};
-export const dislike = (req, res, next) => {};
+import { createError } from "../error.js";
+import User from "../models/User.js";
+
+export const updateUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(createError(403, "You can only update your account!"));
+  }
+};
+
+export const deleteUser = async (req, res, next) => {
+  if (req.params.id === req.user.id) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted!async");
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(createError(403, "You can only delete your account!"));
+  }
+};
+export const getUser = async (req, res, next) => {};
+export const subscribe = async (req, res, next) => {};
+export const unsubscribe = async (req, res, next) => {};
+export const like = async (req, res, next) => {};
+export const dislike = async (req, res, next) => {};
--- api/routes/users.js
+++ api/routes/users.js
@@ -7,29 +7,30 @@ import {
   subscribe,
   unsubscribe,
   updateUser,
-} from "../controllers/user";
+} from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
 //Update user
-router.put("/:id", updateUser);
+router.put("/:id", verifyToken, updateUser);
 
 //Delete user
-router.delete("/:id", deleteUser);
+router.delete("/:id", verifyToken, deleteUser);
 
 //Get user
 router.get("/find/:id", getUser);
 
 //Subscribe a user
-router.put("/sub/:id", subscribe);
+router.put("/sub/:id", verifyToken, subscribe);
 
 //Unsubscribe a user
-router.put("/unsub/:id", unsubscribe);
+router.put("/unsub/:id", verifyToken, unsubscribe);
 
 //Like a video
-router.put("/like/:videoId", like);
+router.put("/like/:videoId", verifyToken, like);
 
 //Dislike a video
-router.put("/dislike/:videoId", dislike);
+router.put("/dislike/:videoId", verifyToken, dislike);
 
 export default router;
