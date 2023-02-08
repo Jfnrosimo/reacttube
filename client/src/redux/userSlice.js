445730d99709d4fd38c2d34fb252928b213cbc87
Jfnrosimo <j.rosimo098.com>
2023-02-08 13:23:35 +0800

    Feat: added user reducer actions

--- client/src/redux/userSlice.js
+++ client/src/redux/userSlice.js
@@ -8,12 +8,27 @@ const initialState = {
 
 export const userSlice = createSlice({
   name: "user",
-
   initialState,
-  reducers: {},
+  reducers: {
+    loginStart: (state) => {
+      state.loading = true;
+    },
+    loginSuccess: (state, action) => {
+      state.loading = false;
+      state.user = action.payload;
+    },
+    loginFail: (state) => {
+      state.loading = false;
+      state.error = true;
+    },
+    logout: (state) => {
+      return initialState;
+    },
+  },
 });
 
 // Action creators are generated for each case reducer function
-export const { increment } = userSlice.actions;
+export const { loginStart, loginSuccess, loginFail, logout } =
+  userSlice.actions;
 
 export default userSlice.reducer;
